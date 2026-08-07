6a813226d2b8629cc7aeadc8a6b3b67c5a991b83
hanhhle <hoanganhle03201208@gmail.com>
2026-08-08 01:19:10 +0700

    Delete pharma_web directory

--- pharma_web/app/models/UserModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../../config/database.php';
-
-class UserModel {
-    public static function getUserProfile($userId = 1) {
-        $mock = Database::getMockData();
-        return $mock['user'];
-    }
-}
--- pharma_web/pharmacity_digital_transformation/README.md
+++
@@ -1,2 +0,0 @@
-# pharmacity_digital_transformation
-
